e32836c42cf17aa5998e56776daf4790a8595dab
Takashi Nakamoto <takashi.nakamoto@nao.ac.jp>
2017-03-08 15:10:18 +0000

    Adding a new component to the existing container.

---
+++ LGPL/CommonSoftware/jcontexmpl/src/alma/demo/JBaciDemoImpl/JBaciDemoImpl.java
@@ -0,0 +1,93 @@
+/*
+ * ALMA - Atacama Large Millimiter Array (c) European Southern Observatory,
+ * 2002 Copyright by ESO (in the framework of the ALMA collaboration), All
+ * rights reserved
+ * 
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your
+ * option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package alma.demo.JBaciDemoImpl;
+import java.util.logging.Logger;
+
+import org.omg.CORBA.DoubleHolder;
+import org.omg.CORBA.IntHolder;
+
+import alma.ACS.ComponentStates;
+import alma.acs.component.ComponentLifecycle;
+import alma.acs.container.ContainerServices;
+import alma.demo.JBaciDemoOperations;
+
+/**
+ * TODO: fill out this JavaDoc comment.
+ * 
+ * @author tnakamoto
+ */
+public class JBaciDemoImpl implements ComponentLifecycle, JBaciDemoOperations
+{
+	private ContainerServices m_containerServices;
+	private Logger m_logger;
+
+	/////////////////////////////////////////////////////////////
+	// Implementation of ComponentLifecycle
+	/////////////////////////////////////////////////////////////
+	
+	public void initialize(ContainerServices containerServices) {
+		m_containerServices = containerServices;
+		m_logger = m_containerServices.getLogger();
+		m_logger.info("initialize() called...");
+	}
+    
+	public void execute() {
+		m_logger.info("execute() called...");
+	}
+    
+	public void cleanUp() {
+		m_logger.info("cleanUp() called..., nothing to clean up.");
+	}
+    
+	public void aboutToAbort() {
+		cleanUp();
+		m_logger.info("managed to abort...");
+        System.out.println("JBaciDemo component managed to abort... you should know this even if the logger did not flush correctly!");
+	}
+	
+	/////////////////////////////////////////////////////////////
+	// Implementation of ACSComponent
+	/////////////////////////////////////////////////////////////
+	
+	public ComponentStates componentState() {
+		return m_containerServices.getComponentStateManager().getCurrentState();
+	}
+	public String name() {
+		return m_containerServices.getName();
+	}
+	
+	/////////////////////////////////////////////////////////////
+	// Implementation of JBaciDemoOperations
+	/////////////////////////////////////////////////////////////
+	
+	public String sayHola() {
+		m_logger.info("sayHola called...");
+		return "hola";
+	}
+	
+	public String sayHolaWithParameters(String inString,
+			DoubleHolder inoutDouble, IntHolder outInt) {
+		m_logger.info("sayHola called with arguments inString=" + inString
+				+ "; inoutDouble=" + inoutDouble.value
+				+ ". Will return 'hola'...");
+		outInt.value = (int) Math.round(Math.E * 10000000);
+		return "hola";
+	}
+}

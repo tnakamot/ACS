fd2b95521be15b117c7513d71c324b322408bf49
Takashi Nakamoto <takashi.nakamoto@nao.ac.jp>
2017-03-17 16:02:18 -0300

    First version of the test case for ROdouble alarm.

--- LGPL/CommonSoftware/jbaci/test/jbaci/test/PropertyAlarmTest.java
+++ LGPL/CommonSoftware/jbaci/test/jbaci/test/PropertyAlarmTest.java
@@ -3,13 +3,29 @@ package jbaci.test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import org.omg.CORBA.Context;
+import org.omg.CORBA.ContextList;
+import org.omg.CORBA.DomainManager;
+import org.omg.CORBA.ExceptionList;
+import org.omg.CORBA.NVList;
+import org.omg.CORBA.NamedValue;
 import org.omg.CORBA.ORB;
+import org.omg.CORBA.Object;
+import org.omg.CORBA.Policy;
+import org.omg.CORBA.Request;
+import org.omg.CORBA.SetOverrideType;
 import org.omg.PortableServer.POA;
 import org.omg.PortableServer.POAHelper;
 import org.omg.PortableServer.POAManager;
 
 import si.ijs.maci.Manager;
 import si.ijs.maci.ManagerHelper;
+import alma.ACS.Alarmdouble;
+import alma.ACS.AlarmdoublePOA;
+import alma.ACS.CBDescIn;
+import alma.ACS.CBDescOut;
+import alma.ACS.ROdouble;
+import alma.ACS.Subscription;
 import alma.ACSErr.CompletionHolder;
 import alma.acs.util.ACSPorts;
 import alma.jbaci.AlarmTestServer;
@@ -104,10 +120,90 @@ public class PropertyAlarmTest extends TestCase {
 		destroyCORBA();
 	}
 
+	/*
 	public void testDouble() throws Throwable {
 		CompletionHolder c = new CompletionHolder();
-		double d = testServer.doubleProp().get_sync(c);
+		double d = 0.0;
+		for (int i = 0; i < 30; i++) {
+			d = testServer.doubleProp().get_sync(c);
+			System.out.println("d = " + d + " (timestamp = " + c.value.timeStamp +")");
+			Thread.sleep(100);
+		}
 		assertEquals(d, -1.0);
+	}
+	*/
+	
+	public void testAlarmRODouble() throws Throwable {
+		ROdouble prop = testServer.doubleProp();
+		
+		double alarm_high_on  = prop.alarm_high_on();
+		double alarm_high_off = prop.alarm_high_off();
+		double alarm_low_on   = prop.alarm_low_on();
+		double alarm_low_off  = prop.alarm_low_off();
+		AlarmCounter counter = new AlarmCounter();
+		
+		System.out.println("Configuration of testAlarmRODouble:");
+		System.out.println("alarm_high_on: " + alarm_high_on);
+		System.out.println("alarm_high_off: " + alarm_high_off);
+		System.out.println("alarm_low_on: " + alarm_low_on);
+		System.out.println("alarm_low_off: " + alarm_low_off);
+				
+		AlarmdoublePOA cbPOA = new AlarmdoublePOA() {
+			public void alarm_raised(double value, alma.ACSErr.Completion c, alma.ACS.CBDescOut desc) {
+				assertTrue(value <= alarm_low_on || alarm_high_on <= value);
+				counter.incrementRaised();
+				System.out.println("alarm_raised: " + value);
+			}
+			public void alarm_cleared(double value, alma.ACSErr.Completion c, alma.ACS.CBDescOut desc) {
+				assertTrue(alarm_low_off <= value && value <= alarm_high_off);
+				counter.incrementCleared();
+				System.out.println("alarm_cleared: " + value);
+			}
+			@Override
+			public boolean negotiate(long time_to_transmit, CBDescOut desc) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+		};
+		
+		// Timeout is set to 1 second.
+		CBDescIn desc = new CBDescIn(10000000, 10000000, 0);
+		Subscription sub = testServer.doubleProp().new_subscription_Alarm(cbPOA._this(orb), desc);
+		
+		// Wait 0.05 second.
+		Thread.sleep(50);
+		
+		// Check if either alarm_raised() or alarm_cleared() is called only once.
+		// By the BACI specification, the BACI property has to send an alarm event 
+		// immediately after the client has subscribed to the alarm.
+		//
+		// As of 17 March, 2017, it was found that C++ BACI implementation cannot pass this
+		// test, so it is commented out for now.
+		
+		// assertEquals(1, counter.getClearedCount() + counter.getRaisedCount());
+		
+		sub.resume();
+		
+		CompletionHolder c = new CompletionHolder();
+		double d = 0.0;
+		for (int i = 0; i < 30; i++) {
+			d = testServer.doubleProp().get_sync(c);
+			System.out.println("d = " + d + " (timestamp = " + c.value.timeStamp +")");
+			Thread.sleep(100);
+		}
+		sub.suspend();
+		sub.destroy();
+		
+		System.out.println("raised_count: " + counter.getRaisedCount());
+		System.out.println("cleared_count: " + counter.getClearedCount());
+
+		// Check if alarm is cleared and raised, at least, two times.
+		// If it isn't, it is considered that this test case does not
+		// test the alarm functionality, and it fails. This is probably
+		// because the configuration in CDB is wrong, so if this test
+		// case fails, fix AlarmTestServerImpl.xml in CDB.
+		assertTrue(counter.getClearedCount() >= 2);
+		assertTrue(counter.getRaisedCount()  >= 2);
 	}
 
 	public static TestSuite suite() {
